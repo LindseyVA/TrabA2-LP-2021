a34fa718be1259f03c030f696909f5280a28339c
LindseyVA <84726458+LindseyVA@users.noreply.github.com>
2021-11-30 23:23:21 -0300

    Update urls.py

--- MAC/MAC/urls.py
+++ MAC/MAC/urls.py
@@ -14,8 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, incude
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('Me_Ajuda_Chef/', include('Me_Ajuda_Chef.urls'))
 ]
