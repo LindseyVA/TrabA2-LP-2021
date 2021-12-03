57edb059aebe0c3c8337fcf2bda94a78d05e1a92
Lindsey <lindseyanjo@gmail.com>
2021-12-03 00:47:18 -0300

    blocos e static

--- MAC/Me_Ajuda_Chefe/urls.py
+++ MAC/Me_Ajuda_Chefe/urls.py
@@ -2,5 +2,11 @@ from django.urls import path
 from Me_Ajuda_Chefe import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('home/', views.home, name='home'),
+    path('search/', views.search, name='search'),
+    path('receita/<str:param>', views.receita, name='receita'),
+    path('produtos/', views.produtos, name='produtos'),
+    path('minhas-receitas/', views.minhas_receitas, name='minhas-receitas'),
+    path('receitas/', views.receitas, name='receitas')
 ]
--- MAC/Me_Ajuda_Chefe/views.py
+++ MAC/Me_Ajuda_Chefe/views.py
@@ -1,7 +1,40 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.urls import reverse
+from django.http import HttpResponse, HttpResponseRedirect, Http404
+
+def home(request):
+  return render(request, 'MAC/home.html')
 
 def index(request):
-  return HttpResponse('<h1>Só testando aqui</h1>')
+    url_redirecionamento = reverse('home')
+    return HttpResponseRedirect(url_redirecionamento)
+
+def search(request):
+  return render(request, 'MAC/search.html')
+
+def receitas(request):
+  return render(request, 'MAC/receitas.htm')
+
+def produtos(request):
+  return render(request, 'MAC/produtos.html')
+
+def minhas_receitas(request):
+  return render(request, 'MAC/minhas-receitas.html')
+
+def receita(request, param):
+  if param == 'pao':
+    return render(request, 'MAC/pao.htm')
+  elif param == 'lasanha-de-carne-moida':
+    return render(request, 'MAC/lasanha_de_carne_moida.htm')
+  elif param == 'fricasse-vegetariano':
+    return render(request, 'MAC/fricasse_vegetariano.htm')
+  elif param == 'capuccino':
+    return render(request, 'MAC/capuccino.html')
+  else:
+    raise Http404()
+
+
+
+
 
 
